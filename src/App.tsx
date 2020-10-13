aa271cdadd65a62a138f2785ee955ac46123df35
Nathan Milhomen <nathanmilhomen07@hotmail.com>
2020-10-12 21:28:35 -0300

    página principal estilizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import logoImg from "./images/logo.svg"
+import { FiArrowRight } from "react-icons/fi"
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!! </h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Logo" />
+
+        <main>
+          <h1>Leve felicidade para o mundo! </h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>Goiás</strong>
+          <span>Anápolis</span>
+        </div>
+
+        <a href="" className="enter-app"> <FiArrowRight size={26} color="rgba=(0, 0, 0, 0.6)" /> </a>
+      </div>
     </div>
   );
 }
