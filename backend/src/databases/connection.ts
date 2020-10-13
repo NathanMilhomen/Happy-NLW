f72d3d813e6e3bab80869a671e73ecfd586f58ab
Nathan Milhomen <nathanmilhomen07@hotmail.com>
2020-10-13 12:55:36 -0300

    orm

---
+++ backend/src/databases/connection.ts
@@ -0,0 +1,10 @@
+// import { Sequelize } from 'sequelize';
+
+// const sequelize = new Sequelize('happy', 'root', 'password', {
+//     host: 'localhost',
+//     dialect: 'mssql'
+// });
+
+import { createConnection } from "typeorm";
+
+createConnection();
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,15 @@
 import express from "express";
 
+import "./databases/connection";
+
 const app = express();
+app.use(express.json());
+
+app.get("/user", (request, response) => {
+    response.send("Quero que tu vá, vá ")
+})
+
+
+
 
 app.listen(3333);
